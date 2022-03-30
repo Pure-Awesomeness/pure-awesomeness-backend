3e525d88c1eb08e9e6abfafaa47b70f934f49c66
tapankumar23 <sabat.tapan@gmail.com>
2022-03-31 01:50:39 +0800

    First GraphQL Commit

---
+++ src/main/java/com/pure/awsm/resolver/BankAccountResolver.java
@@ -0,0 +1,23 @@
+package com.pure.awsm.resolver;
+
+import com.pure.awsm.domain.bank.BankAccount;
+import com.pure.awsm.domain.bank.Currency;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class BankAccountResolver implements GraphQLQueryResolver {
+
+    public BankAccount bankAccount(UUID id) {
+        log.info("Retrieving bank account ");
+
+        return BankAccount.builder()
+                .id(id)
+                .currency(Currency.USD)
+                .build();
+    }
+}
