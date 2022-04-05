39880e02276a1de8535ef8a1792df0ad0aa964fe
tapankumar23 <sabat.tapan@gmail.com>
2022-04-05 14:40:42 +0800

    docker added

--- src/main/java/com/pure/awsm/Application.java
+++ src/main/java/com/pure/awsm/Application.java
@@ -1,13 +1,20 @@
 package com.pure.awsm;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> shutdownHook()));
     }
 
+    private static void shutdownHook() {
+        log.info("Inside Shutdown Hook..");
+        log.info("Shutdown Cleanup Done");
+    }
 }
